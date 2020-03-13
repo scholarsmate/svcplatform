afa166afe921977ad8e92bf8c31c5d47034f66d6
Davin Shearer <davin.shearer@evc.ctc.com>
2020-03-13 11:02:58 -0400

    Make the rsync backup installation optional

--- platform/scripts/setup_rsync.sh
+++ platform/scripts/setup_rsync.sh
@@ -1,34 +1,27 @@
 #!/usr/bin/env bash
 
-RSYNC_BACKUP_SERVER=${RSYNC_BACKUP_SERVER:-192.168.1.45}
-RSYNC_BACKUP_USER=${RSYNC_BACKUP_USER:-devops}
-
 echo '##########################################################################'
 echo '#               About to run setup_rsync.sh script                       #'
 echo '##########################################################################'
 
 set -ex
 
-# install nfs server software
-yum install -y incron
+# Only setup the rsync backups if both of these environment variables are populated
+if [[ -n "${RSYNC_BACKUP_SERVER}" && -n "${RSYNC_BACKUP_USER}"]]; then
+    # install nfs server software
+    yum install -y incron
 
-# setup incron table
-# NOTE: The vagrant public key must be authorized on this system for this user for the rsync to work
-echo "/nfs/export_rw/backups  IN_CLOSE_WRITE  rsync -a /nfs/export_rw/backups/ ${RSYNC_BACKUP_USER}@${RSYNC_BACKUP_SERVER}:/home/devops/backups" | tee -a /var/spool/incron/vagrant
+    # setup incron table
+    # NOTE: The vagrant public key must be authorized on this system for this user for the rsync to work
+    echo "/nfs/export_rw/backups  IN_CLOSE_WRITE  rsync -a /nfs/export_rw/backups/ ${RSYNC_BACKUP_USER}@${RSYNC_BACKUP_SERVER}:/home/${RSYNC_BACKUP_USER}/backups" | tee -a /var/spool/incron/vagrant
 
-# enable and start the incrond service
-systemctl enable incrond
-systemctl start incrond
+    # enable and start the incrond service
+    systemctl enable incrond
+    systemctl start incrond
 
-# allow rsyncd traffic through the firewall
-systemctl start firewalld
-systemctl enable firewalld
-firewall-cmd --permanent --zone=public --add-service=rsyncd
-systemctl restart firewalld
-
-# create a shared directory for the vagrant user
-mkdir -p /nfs/export_rw/vagrant
-chown vagrant:vagrant /nfs/export_rw/vagrant
-
-# confirm the exports
-showmount -e localhost
+    # allow rsyncd traffic through the firewall
+    systemctl start firewalld
+    systemctl enable firewalld
+    firewall-cmd --permanent --zone=public --add-service=rsyncd
+    systemctl restart firewalld
+fi
